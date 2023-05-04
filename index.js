9787f81ef8268a63ee57f067d3e9e3b8bf8cc970
Walter Park <neodust223@gmail.com>
2023-05-04 14:30:20 +0900

    #2.4 완료
    nodemon을 설치해서 콘솔에서 매번 실행 명령을 치지 않고 변경사항이 있을 때마다 런타임이 재실행되도록 했다.
    package.json 파일 스크립트에서 index.js를 실행하는 명령어가 node -> babel-node -> nodemon --exec babel-node로 변경되어가는 과정에 주목할 것.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require("express");
+import express from "express";  // express를 가져오는 최신 문법. 이 코드를 babel이 아래 코드로 변환해준다.
+//const express = require("express");
 
 const app = express();
 
-console.log("Hi!");
+console.log("I'm restarting");
