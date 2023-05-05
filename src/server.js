1f6b3bcab61f4f605e3b3bc8355416c6574d6a01
Walter Park <neodust223@gmail.com>
2023-05-06 01:17:44 +0900

    #3.0 complite
    launching my first server!!!!

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";  // express를 가져오는 최신 문법. 이 코드를 babel이 아래 코드로 변환해준다.
 //const express = require("express");
 
+const PORT = 4000;
 const app = express();
 
-console.log("I'm restarting");
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
+
+app.listen(PORT, handleListening);
