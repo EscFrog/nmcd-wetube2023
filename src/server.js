d823cbb18d3af36f0e96c397ee52e2b3cf505329
Walter Park <neodust223@gmail.com>
2023-05-06 22:17:42 +0900

    Update server.js
    겟 메소드 추가

--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@ import express from "express";  // express를 가져오는 최신 문법. 이 �
 //const express = require("express");
 
 const PORT = 4000;
+
 const app = express();
+
+// get 메소드는 콜백 함수를 필요로한다. 매개 변수는 두 개가 필요한데, 각각 request, respons object를 받는다.
+app.get("/", (req, res) => {return res.send("Did you ask something?")});
+// app.get("/", (req, res) => {return res.end()});
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
 
