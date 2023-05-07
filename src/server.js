d3f68bafb8c44a509e62122aa8cb704a71adb185
Walter Park <neodust223@gmail.com>
2023-05-07 13:56:29 +0900

    #3.5 완료
    미들웨어를 추가해서 경유하는 법

--- src/server.js
+++ src/server.js
@@ -5,9 +5,17 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next(); // 미들웨어 함수에서 다음 함수로 넘어갈 때 쓰는 함수.
+}
+
+const handleHome = (req, res) => {
+    return res.send("Route Finish!!!");
+};
+
 // get 메소드는 콜백 함수를 필요로한다. 매개 변수는 두 개가 필요한데, 각각 request, respons object를 받는다.
-app.get("/", (req, res) => {return res.send("Did you ask something?")});
-// app.get("/", (req, res) => {return res.end()});
+app.get("/", gossipMiddleware, handleHome); // 미들웨어를 몇 개든 추가할 수 있음.
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
 
