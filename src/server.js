12bf79422f7e7a7a9c04c7d22a2318e32079155b
Walter Park <neodust223@gmail.com>
2023-05-07 14:30:26 +0900

    #3.6 완료
    all.use() 펑션을 이용해서 전역적으로 미들웨어를 경유하게 하는 법 공부. 만약 미들웨어가 중간에 next() 함수를 호출하지 않거나 return res.send() 함수로 결과를 반환해버리면 경유가 끝난다.

--- src/server.js
+++ src/server.js
@@ -5,18 +5,34 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next(); // 미들웨어 함수에서 다음 함수로 넘어갈 때 쓰는 함수.
+}
+
+const privateMW = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("It's ok, you can pass through");
+    next();
 }
 
 const handleHome = (req, res) => {
     return res.send("Route Finish!!!");
 };
 
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
+
+app.use(logger);
+app.use(privateMW);
 // get 메소드는 콜백 함수를 필요로한다. 매개 변수는 두 개가 필요한데, 각각 request, respons object를 받는다.
-app.get("/", gossipMiddleware, handleHome); // 미들웨어를 몇 개든 추가할 수 있음.
+app.get("/", handleHome);
+app.get("/protected", handleProtected); // ~/protected 라는 주소로 가려고 하면, 원래는 이 부분이 실행되어 "Welcome to the private lounge." 라는 메시지가 나와야 하지만, 위에서 privateMW 미들웨어가 막아버려서 올 수 없다.
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening);  // express를 시작하고 본격적으로 대기 상대로 만든다.
