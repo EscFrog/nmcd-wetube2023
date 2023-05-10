9e1a52ccae5ae184d716a4ca0be37f9c714d50da
Walter Park <neodust223@gmail.com>
2023-05-10 22:15:22 +0900

    4.8 완료
    URL에 파라미터 넣는 법

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,8 @@ export const trending = (req, res) => res.send("Home Page Videos")
 export const search = (req, res) => res.send("Search");
 
 export const upload = (req, res) => res.send("Upload Video");
-export const see = (req, res) => res.send("See Video");
+export const see = (req, res) => {
+    return res.send(`See Video #${req.params.id}`);
+};
 export const edit = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,6 +4,7 @@ import {upload, see, edit, deleteVideo} from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
+// :은 파라미터라는 표시. 파라미터를 사용하는 라우터가 그렇지 않은 라우터 위로 올라가면, url의 일부를 변수로 착각할 수 있으므로 순서에 주의해야 한다.
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
