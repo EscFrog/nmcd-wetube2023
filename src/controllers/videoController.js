8eca13a55a2cda680e36e8c4aeb3af61094f6c8e
Walter Park <neodust223@gmail.com>
2023-05-13 14:39:09 +0900

    add edit page
    edit page 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,5 +3,5 @@ export const search = (req, res) => res.send("Search");
 
 export const upload = (req, res) => res.send("Upload Video");
 export const see = (req, res) => res.render("watch")    // 파일명에 띄어쓰기가 있으면 안되고 언제나 소문자여야 한다.
-export const edit = (req, res) => res.send("Edit Video");
+export const edit = (req, res) => res.render("edit");
 export const deleteVideo = (req, res) => res.send("Delete Video");
