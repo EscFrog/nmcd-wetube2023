97b62be47ae9121cf74cfa4bb02daf367c807830
Walter Park <neodust223@gmail.com>
2023-05-13 14:30:20 +0900

    partial footer
    footer 부분을 별도의 파일로 분리

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,6 @@ export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search");
 
 export const upload = (req, res) => res.send("Upload Video");
-export const see = (req, res) => res.render("watch")
+export const see = (req, res) => res.render("watch")    // 파일명에 띄어쓰기가 있으면 안되고 언제나 소문자여야 한다.
 export const edit = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
