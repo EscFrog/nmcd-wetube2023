a2320c155543e44a7e50dd6b8a38e6759908f330
Walter Park <neodust223@gmail.com>
2023-05-14 00:26:48 +0900

    5.4 완료
    타이틀에 변수를 넣고 비디오 컨트롤러에서 값 전닳하는 식으로 페이지 타이틀을 변경함

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle : "Home"});
 export const search = (req, res) => res.send("Search");
 
 export const upload = (req, res) => res.send("Upload Video");
-export const see = (req, res) => res.render("watch")    // 파일명에 띄어쓰기가 있으면 안되고 언제나 소문자여야 한다.
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) => res.render("watch", {pageTitle : "Watch"});    // 파일명에 띄어쓰기가 있으면 안되고 언제나 소문자여야 한다.
+export const edit = (req, res) => res.render("edit", {pageTitle : "Edit"});
 export const deleteVideo = (req, res) => res.send("Delete Video");
