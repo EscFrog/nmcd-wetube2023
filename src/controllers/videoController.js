158b74f1d141744aa4dac6f32f42caff0cc5ede1
Walter Park <neodust223@gmail.com>
2023-05-23 00:12:08 +0900

    가짜 비디오 오브젝트 배열 삭제
    진짜 데이터베이스를 사용하기 위해, 임시로 만들었던 가짜 데이터 오브젝트와 관련 코드를 삭제

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,52 +1,20 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 0,
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 1,
-        id: 1,
-    },
-    {
-        title: "Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 2,
-    }
-];
-
-export const trending = (req, res) => {
-    return res.render("home", { pageTitle : "Home", videos})
+export const home = (req, res) => {
+    return res.render("home", { pageTitle : "Home"})
 };
 
 export const watch = (req, res) => {
     const {id} = req.params;
-    // 위 코드는 ES6 문법으로서 아래 코드와 같다.
-    // const id = req.params.id;
-    const video = videos[id];
-    return res.render("watch", {pageTitle : `Watching: ${video.title}`, video});
+    return res.render("watch", {pageTitle : `Watching`});
 };
 
 export const getEdit = (req, res) => {
     const {id} = req.params;
-    const video = videos[id];
-    return res.render("edit", {pageTitle : `Editing: ${video.title}`, video});
+    return res.render("edit", {pageTitle : `Editing`});
 };
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { newTitle } = req.body;
-    videos[id].title = newTitle;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -55,16 +23,6 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-    // here i will add a video to the videos array.
-    const newVideo = {
-        title: req.body.videoTitle,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        views: 0,
-        id: videos.length,
-    }
-    console.log(newVideo);
-    videos.push(newVideo);
+    const {title} = req.body;
     return res.redirect("/");
 }
