5f750f0297dce27eec5da8e6f888e83b8f16adaa
Walter Park <neodust223@gmail.com>
2023-05-25 00:04:38 +0900

    6.13 완료
    async-await 패턴을 이용해 데이터베이스에서 데이터를 불러오고, 데이터를 불러오는 데 실패했을 때 에러 처리까지 하는 코드.
    최신 몽구스에서는 콜백을 지원하지 않지만 참고를 위해 주석 내에 콜백 형식을 작성함.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,25 @@
-export const home = (req, res) => {
-    return res.render("home", { pageTitle : "Home"})
+import Video from "../models/Video";
+
+/*
+// 이제 몽구스는 callback 패턴을 사용하지 않고 await async 패턴만 사용하지만
+// 만약 callback 패턴을 사용할 수 있었다면 다음과 같이 코드를 짤 수 있다.
+Video.find({}, (error, videos) => {
+    if (error) {
+        return res.render("server-error");
+    }
+    return res.render("home", { pageTitle: "Home", videos });
+});
+*/
+
+
+export const home = async(req, res) => {
+    try {
+        const videos = await Video.find({});
+        return res.render("home", { pageTitle : "Home", videos })
+    } catch {
+        return res.rander("server-error")
+    }
+    
 };
 
 export const watch = (req, res) => {
