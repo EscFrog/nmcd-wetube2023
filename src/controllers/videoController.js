03c8448d95016d4ab8f7a31deac347c1c37c65b2
Walter Park <neodust223@gmail.com>
2023-05-28 20:28:50 +0900

    try catch를 이용해 에러 잡아내기
    데이터 형식이 올바르지 않을 경우 에러가 출력되도록 변경

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,15 +36,22 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
     const {videoTitle, description, hashtags} = req.body;
-    await Video.create({
-        title: videoTitle,
-        description,
-        createdAt: Date.now(),
-        hashtags: hashtags.replace(/\s/g, "").split(",").map((word) => `#${word}`), // hashtags 값에서, 공백을 제거하고, ","를 기준으로 분할한 후, "#"문자를 붙여 배열로 반환한다.
-        meta: {
-            views: 0,
-            rating: 0,
-        }
-    });
-    return res.redirect("/");
-}
+	try{
+    	await Video.create({
+    	    title: videoTitle,
+    	    description,
+    	    createdAt: Date.now(),
+    	    hashtags: hashtags.replace(/\s/g, "").split(",").map((word) => `#${word}`), // hashtags 값에서, 공백을 제거하고, ","를 기준으로 분할한 후, "#"문자를 붙여 배열로 반환한다.
+    	    meta: {
+    	        views: 0,
+    	        rating: 0,
+    	    }
+    	});
+    	return res.redirect("/");
+	} catch {
+		return res.render("upload",{
+			pageTitle: "Upload Video",
+			errorMessage: error._message,
+		});
+	};
+};
