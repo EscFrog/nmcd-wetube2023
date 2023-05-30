3cfc86a579b213d9bf42638e639de116386ee708
Walter Park <neodust223@gmail.com>
2023-05-30 15:18:30 +0900

    schema에 제한사항추가
    프론트엔드 사이드와 백엔드 사이드 양쪽에 제한을 적용한다. 그래야 어느 한쪽에서 보안을 깨더라도 피해가 적다.

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
   // 위 코드는 축약형. 원래는 title: { type: String },
-  description: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, default: Date.now },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0 },
     rating: { type: Number, default: 0 },
