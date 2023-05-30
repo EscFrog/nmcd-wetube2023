6899b32ec8b7a8e667895ee7ceafec934e5ff865
Walter Park <neodust223@gmail.com>
2023-05-30 15:33:03 +0900

    Update videoRouter.js
    라우터에 사용된 id의 정규표현식을 몽고DB의 아이디 형식(24자리의 16진수 숫자)에 맞도록 수정.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,12 +9,11 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 // 위 코드는 아래 두 줄의 코드를 축약한 것이다. 이를 체인 메서드 패턴이라고 하는데, 객체의 메소드가 객체 자신을 반환하기 때문에 메소드를 이어서 호출할 수 있는 것이다.
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit)
-
-videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
