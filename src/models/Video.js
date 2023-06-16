835090f60ea22b911dc2c9323cda5fcf1554764d
Walter Park <neodust223@gmail.com>
2023-06-16 19:00:38 +0900

    6.24 중간커밋
    함수를 만들어서 export하는 방식이 아니라 아예 Video에 포함된 메서드(?)처럼 만드는 방법.

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-export const formatHashtags = (hashtags) =>
-  hashtags
-    .replace(/\s/g, "")
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   // 위 코드는 축약형. 원래는 title: { type: String },
@@ -16,6 +10,13 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, default: 0 },
     rating: { type: Number, default: 0 },
   },
+});
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .replace(/\s/g, "")
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
