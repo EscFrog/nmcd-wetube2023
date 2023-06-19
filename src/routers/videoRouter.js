9827a50a4fd77601458a87bb74e5c925108fbe04
Walter Park <neodust223@gmail.com>
2023-06-19 12:31:05 +0900

    6.25 delete video 완료
    watch 페이지에서 링크로 id/delete 페이지로 보내면, 비디오 라우터에서 get 명령어로 controller에 있는 deleteVideo 명령어를 실행한다.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -15,5 +16,6 @@ videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 // 위 코드는 아래 두 줄의 코드를 축약한 것이다. 이를 체인 메서드 패턴이라고 하는데, 객체의 메소드가 객체 자신을 반환하기 때문에 메소드를 이어서 호출할 수 있는 것이다.
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit)
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 
 export default videoRouter;
