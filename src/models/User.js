3b3d36561f36a052272898408f5eb951da9f0e59
Walter Park <neodust223@gmail.com>
2023-06-22 13:01:36 +0900

    #7.2 완료 (패스워드 해싱)
    미들웨어로 bcrypt를 추가해 User 데이터를 저장하기 전에 패스워드를 암호화하는 과정을 추가

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  console.log("Users password:", this.password);
+  this.password = await bcrypt.hash(this.password, 5); // 마지막 숫자는 saltRounds, 즉 해싱을 몇 번 반복할 것인가를 의미...
+  console.log("Hashed password:", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
