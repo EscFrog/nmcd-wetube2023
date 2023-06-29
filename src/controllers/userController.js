c1fb0ae65b880ebfaedd9790865f66f5aab37746
Walter Park <neodust223@gmail.com>
2023-06-29 11:13:31 +0900

    #7.5 중간커밋
    로그인 페이지 만들기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,7 +33,8 @@ export const postJoin = async (req, res) => {
     });
   }
 };
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) => res.send("Log out");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
