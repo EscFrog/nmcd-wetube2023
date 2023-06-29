08724f32b17349dc503be1b586749ea07cade8e8
Walter Park <neodust223@gmail.com>
2023-06-29 15:11:41 +0900

    #7.7 완료
    일단 미들웨어에서 세션 사용하는 부분까지만.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express"; // express를 가져오는 최신 문법. 이 코드를 babel이 아래 코드로 변환해준다.
 //const express = require("express");
 import morgan from "morgan";
+import session from "express-session";
 
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -14,6 +15,13 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); // 순서가 중요하다. 비디오 라우터로 가기 전에 이 미들웨어에서 form의 데이터를 인코드한다.
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
