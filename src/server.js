cbfcedac6362ed907e4872068c7abc3bd51f1642
Walter Park <neodust223@gmail.com>
2023-07-03 12:07:21 +0900

    #7.13 Uninitialized Sessions 완료
    세션 정보가 변경될 때만 세션을 DB에 저장하도록 변경. 문서를 좀 더 읽어보도록 하자.

--- src/server.js
+++ src/server.js
@@ -23,8 +23,8 @@ app.use(express.urlencoded({ extended: true })); // 순서가 중요하다. 비�
 app.use(
   session({
     secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
