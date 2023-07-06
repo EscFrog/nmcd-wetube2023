b70057f1690dff2c6a7b3c0fb68dc25073aa14a9
Walter Park <neodust223@gmail.com>
2023-07-06 14:25:13 +0900

    클라이언트ID 위치 변경
    github 인증시 클라이언트 ID를 여러 곳에서 사용할 수 있도록 .env 파일로 옮김

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,7 +65,7 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "db43e6da937093dded41",
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
