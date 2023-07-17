5f980ad8a1f9cca81ebc6973f3dec688b701fdc6
Walter Park <neodust223@gmail.com>
2023-07-17 18:29:34 +0900

    #7.22 완료
    remove user같은 페이지는 필요 없으니 빼고 강의 마무리.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,5 +141,4 @@ export const logout = (req, res) => {
   return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
 export const see = (req, res) => res.send("See User");
