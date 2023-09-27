d67519acf0e28052780705437f1d84902ffe027b
Walter Park <neodust223@gmail.com>
2023-09-27 12:27:25 +0900

    비디오 라우터에도 프로텍트 미들웨어 추가.
    나중에는 비디오를 올리거나, 수정하거나 지우는 것도 로그인 한 유저만 할 수 있도록 할 거라서, 비디오 라우터에도 프로텍터 미들웨어를 추가한다.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,15 +7,25 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-// 위 코드는 아래 두 줄의 코드를 축약한 것이다. 이를 체인 메서드 패턴이라고 하는데, 객체의 메소드가 객체 자신을 반환하기 때문에 메소드를 이어서 호출할 수 있는 것이다.
-// videoRouter.get("/:id(\\d+)/edit", getEdit);
-// videoRouter.post("/:id(\\d+)/edit", postEdit)
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+// 위 코드에서는 체인 메서드 패턴을 사용했다. 객체의 메소드가 객체 자신을 반환하기 때문에 메소드를 이어서 호출할 수 있는 것이다.
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
