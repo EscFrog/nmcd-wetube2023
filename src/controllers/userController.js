3565518c897d8975f4878cb88f202b8db56d9253
Walter Park <neodust223@gmail.com>
2023-10-05 22:44:48 +0900

    #8.7 File Uploads PT.2 완료
    post 할 때, 아바타 파일이 있으면 그 파일 경로로 avatarUrl을 교체해주고, 없으면 현재 세션에 로그인 한 유저의 avatarUrl을 그대로 저장한다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -147,7 +147,7 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
@@ -156,6 +156,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
