f1f4aa8658aea7bd3be55b625d813a35dd012583
Walter Park <neodust223@gmail.com>
2023-10-05 12:32:33 +0900

    #8.7 File Uploads PT.1 완료
    multer라는 미들웨어를 이용해 이미지파일을 서버에 저장하는 과정 완료

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,5 @@
+import multer from "multer";
+
 export const localsMiddleware = (req, res, next) => {
   // session에 저장된 값을 각각 로컬저장소에 저장한다. 이 값은 이제 모든 템플릿에서 가져다 쓸 수 있다.
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -23,3 +25,5 @@ export const publicOnlyMiddleware = (req, res, next) => {
     return res.redirect("/");
   }
 };
+
+export const uploadFiles = multer({ dest: "uploads/" });
