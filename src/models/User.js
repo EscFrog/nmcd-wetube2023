0fca5c6eebd4c4da1e0661fbee456ad57e84e08f
Walter Park <neodust223@gmail.com>
2023-10-08 20:13:44 +0900

    #8.14 Bugfix
    비밀번호를 변경했을 때만 비밀번호가 해시되도록 조건문 추가

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5); // 마지막 숫자는 saltRounds, 즉 해싱을 몇 번 반복할 것인가를 의미...
+  if (this.isModified("password")) {
+    // 패스워드가 변경되었을 때만... 이 조건문을 넣은 이유는 비디오를 저장할 때도 user모델을 DB에 새로 저장하기 때문.
+    this.password = await bcrypt.hash(this.password, 5); // 마지막 숫자는 saltRounds, 즉 해싱을 몇 번 반복할 것인가를 의미...
+  }
 });
 
 const User = mongoose.model("User", userSchema);
