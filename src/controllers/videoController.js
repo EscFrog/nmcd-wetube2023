83b65f8c8ceddb6c0ce2db0406eb04030df1b523
Walter Park <neodust223@gmail.com>
2023-10-10 12:59:25 +0900

    Update videoController.js
    postEdit에서 비디오를 찾지 못해 업데이트가 안되는 버그 해결

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -38,7 +38,7 @@ export const postEdit = async (req, res) => {
     user: { _id },
   } = req.session;
   const { newTitle, newDescription, newHashtags } = req.body;
-  const video = await Video.exists({ _id: id });
+  const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
