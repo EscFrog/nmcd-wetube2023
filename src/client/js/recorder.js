df8aa335a4c72f981915ebe84e7c4b848d015497
Walter Park <neodust223@gmail.com>
2023-11-06 11:43:16 +0900

    #13.3 Recording Video pt.2
    recorder를 전역변수로 빼고, stop recording 버튼을 눌러서 녹화를 종료할 수 있도록 변경. 그리고 쓸데없는 로그와 코드들을 지움.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,29 +2,24 @@ const startBtn = document.getElementById("startBtn");
 const previewScreen = document.getElementById("preview");
 
 let stream;
+let recorder;
 
 const handleStopRecording = () => {
   startBtn.innerText = "Start Recording";
   startBtn.removeEventListener("click", handleStopRecording);
   startBtn.addEventListener("click", handleStartRecording);
+  recorder.stop();
 };
 
 const handleStartRecording = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStartRecording);
   startBtn.addEventListener("click", handleStopRecording);
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
-    console.log("recording done");
-    console.log(event);
     console.log(event.data);
   };
-  console.log(recorder);
   recorder.start();
-  console.log(recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 5000);
 };
 
 const init = async () => {
