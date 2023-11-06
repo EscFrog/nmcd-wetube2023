fb5f22b5a622d4a384373b7c1241b641f91e6f3b
Walter Park <neodust223@gmail.com>
2023-11-06 11:39:29 +0900

    #13.3 Recording Video pt.1
    녹화를 시작하고 멈추는 이벤트 리스너와 핸들러를 만들었다. stream을 전역변수로 바꾸고, MediaRecorder라는 객체를 생성해 거기에 저장한다.
    저장 상태를 확인하기 위한 Log를 여러개 추가했다.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,34 @@
 const startBtn = document.getElementById("startBtn");
 const previewScreen = document.getElementById("preview");
 
-const handleRecord = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStopRecording = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStopRecording);
+  startBtn.addEventListener("click", handleStartRecording);
+};
+
+const handleStartRecording = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStartRecording);
+  startBtn.addEventListener("click", handleStopRecording);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("recording done");
+    console.log(event);
+    console.log(event.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 400, height: 300 },
   });
@@ -10,4 +36,6 @@ const handleRecord = async () => {
   previewScreen.play();
 };
 
-startBtn.addEventListener("click", handleRecord);
+init();
+
+startBtn.addEventListener("click", handleStartRecording);
